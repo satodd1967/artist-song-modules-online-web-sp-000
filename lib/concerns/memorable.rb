57bc43807e10efc206161bf13c340703d71aaab9
Seth Todd <seth.todd@outlook.com>
2020-03-30 01:26:36 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,15 @@
 module Memorable
+  module ClassMethods
   
-  def reset_all
-    self.all.clear
-  end
+    def reset_all
+      self.all.clear
+    end
 
-  def count
-    self.all.count
+    def count
+     self.all.count
+    end
   end
   
+  module InstanceMethods
+end
 end
